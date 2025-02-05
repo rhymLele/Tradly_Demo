7830ed248e43aaea98c15b31cd21ad96ed6420de
rhymLele <sympathic.elf181203@gmail.com>
2025-02-05 11:12:58 +0700

    101%

--- app/src/main/java/com/duc/tradly/Detail/DetailActivity.kt
+++ app/src/main/java/com/duc/tradly/Detail/DetailActivity.kt
@@ -31,44 +31,44 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right,systemBars.bottom)
             insets
         }
-        val bottomSheet = findViewById<View>(R.id.bottom_sheet)
-        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
-
-        bottomSheetBehavior.peekHeight = 300
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
-        val bt=findViewById<Button>(R.id.btn_keolen)
-        bottomSheetBehavior.addBottomSheetCallback(object :
-            BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                // Không cần xử lý gì ở đây nếu chỉ giới hạn chiều cao
-                when (newState) {
-                    BottomSheetBehavior.STATE_COLLAPSED -> {
-                        bottomSheetBehavior.peekHeight = 300
-                        Log.d("height",bottomSheetBehavior.state.toString())
-                        bt.visibility=View.VISIBLE
-                    }
-
-                    BottomSheetBehavior.STATE_EXPANDED -> {
-                        bt.visibility=View.INVISIBLE
-                        bottomSheetBehavior.maxHeight = 600
-                        Log.d("height1",bottomSheetBehavior.state.toString())
-                    }
-                }
-            }
-
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {
-            }
-        })
-
-        val mViewPager2:ViewPager2
-        val mCircleIndicator3:CircleIndicator3
+//        val bottomSheet = findViewById<View>(R.id.bottom_sheet)
+//        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+//
+//        bottomSheetBehavior.peekHeight = 300
+//        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+//        val bt=findViewById<Button>(R.id.btn_keolen)
+//        bottomSheetBehavior.addBottomSheetCallback(object :
+//            BottomSheetBehavior.BottomSheetCallback() {
+//            override fun onStateChanged(bottomSheet: View, newState: Int) {
+//                // Không cần xử lý gì ở đây nếu chỉ giới hạn chiều cao
+//                when (newState) {
+//                    BottomSheetBehavior.STATE_COLLAPSED -> {
+//                        bottomSheetBehavior.peekHeight = 300
+//                        Log.d("height",bottomSheetBehavior.state.toString())
+//                        bt.visibility=View.VISIBLE
+//                    }
+//
+//                    BottomSheetBehavior.STATE_EXPANDED -> {
+//                        bt.visibility=View.INVISIBLE
+//                        bottomSheetBehavior.maxHeight = 600
+//                        Log.d("height1",bottomSheetBehavior.state.toString())
+//                    }
+//                }
+//            }
+//
+//            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+//            }
+//        })
+//
+//        val mViewPager2:ViewPager2
+//        val mCircleIndicator3:CircleIndicator3
         val btn_back=findViewById<ImageButton>(R.id.btn_back)
-        mViewPager2=findViewById(R.id.view_pager2)
-        mCircleIndicator3=findViewById(R.id.circle_indicator)
+//        mViewPager2=findViewById(R.id.view_pager2)
+//        mCircleIndicator3=findViewById(R.id.circle_indicator)
         val data =getListPhoto()
         val photoViewPagerAdapter= PhotoViewAdapter2(data)
-        mViewPager2.adapter=photoViewPagerAdapter
-        mCircleIndicator3.setViewPager(mViewPager2)
+//        mViewPager2.adapter=photoViewPagerAdapter
+//        mCircleIndicator3.setViewPager(mViewPager2)
         val originalPrice = findViewById<TextView>(R.id.original_price)
         originalPrice.paintFlags = originalPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
         val product = intent.getSerializableExtra("product_data") as? Product
